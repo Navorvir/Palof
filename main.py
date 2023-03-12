6c62645ffcd655fd274365388c4aef474f82649c
pioupiou47 <116309446+pioupiou47@users.noreply.github.com>
2023-03-12 10:32:29 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -38,16 +38,18 @@ while running:
 
     screen.fill((r,g,b))
 
-    bloc1 = pygame.Rect(0,400,200,50)
-    bloc2 = pygame.Rect(500, 400, 200, 50)
-    bloc3 = pygame.Rect(700, 200, 200, 150)
-    bloc4 = pygame.Rect(200, 800, 400, 50)
-    bloc5 = pygame.Rect(0, 650, 200, 150)
-    bloc6 = pygame.Rect(600, 650, 200, 150)
-    bloc7 = pygame.Rect(0, 500, 100, 50)
-    bloc8 = pygame.Rect(400, 500, 100, 200)
+    bloc1 = pygame.Rect(-150,100,200,5000)
+    bloc2 = pygame.Rect(500, 400+500, 200, 50)
+    bloc3 = pygame.Rect(700, 200+500, 200, 150)
+    bloc4 = pygame.Rect(200, 800+500, 400, 50)
+    bloc5 = pygame.Rect(0, 650+500, 200, 150)
+    bloc6 = pygame.Rect(600, 650+500, 200, 150)
+    bloc7 = pygame.Rect(0, 500+500, 100, 50)
+    bloc8 = pygame.Rect(400, 500+500, 100, 200)
+    bloc9 = pygame.Rect(1000, 200+500, 100, 2000)
+    bloc10 = pygame.Rect(50, 190+500, 1000000, 50)
 
-    liste_bloc = [bloc1,bloc2,bloc3,bloc4,bloc5,bloc6,bloc7,bloc8]
+    liste_bloc = [bloc1,bloc2,bloc3,bloc4,bloc5,bloc6,bloc7,bloc8,bloc9,bloc10]
 
     for i in range(len(liste_bloc)):
         pygame.draw.rect(screen,(100+i*11,255-20*i,255 - i*10),liste_bloc[i])
