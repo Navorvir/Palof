09baae216d8d2a8b3f68253ffe522276b631d989
Robin <116309446+pioupiou47@users.noreply.github.com>
2023-04-28 22:16:43 +0200

    Update palourde.py

--- source/Package/Game/palourde.py
+++ source/Package/Game/palourde.py
@@ -91,7 +91,7 @@ class Palourde:
         self.degreeBrasDroit : float = 234
         self.isKicking : bool = False
         self.etapeKick : int = 1
-        self.sante : float = 100
+        self.sante : float = 300
         self.recoitDegat : bool = False
         self.frameRouge : int = 0
         self.mort : bool = False
@@ -501,7 +501,7 @@ class Palourde:
         """
         # Créé par Robin
         self.modeVersus = False
-        self.sante = 100
+        self.sante = 300
         self.mort = False
         self.PALOURDE_IMAGE_RESIZED = pygame.transform.scale(self.PALOURDE_IMAGE, (self.LARGEUR, self.HAUTEUR))
         self.frameRouge = 0
@@ -520,7 +520,7 @@ class Palourde:
         """
         # Créé par Robin
         self.modeVersus = True
-        self.sante = 100
+        self.sante = 300
         self.mort = False
         self.PALOURDE_IMAGE_RESIZED = pygame.transform.scale(self.PALOURDE_IMAGE, (self.LARGEUR, self.HAUTEUR))
 
